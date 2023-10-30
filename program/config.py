2a1efc274b9debb49a464913fb229be66e74bfcb
Jason Fung <x00174641@mytudublin.ie>
2023-10-30 17:08:18 +0000

    Add class for OBS config, added functions and main

---
+++ program/config.py
@@ -0,0 +1,37 @@
+from configparser import ConfigParser
+import os
+
+class OBSConfig:
+    def __init__(self, dir, name):
+        self.ini_path = os.path.join(dir, name)
+        self.config = ConfigParser()
+        with open(self.ini_path, 'r', encoding='utf-8-sig') as f:
+            self.config.read_file(f)
+
+    @property
+    def websocket_status(self):
+        return self.config.get('OBSWebSocket', 'serverenabled')
+
+    @websocket_status.setter
+    def websocket_status(self, value):
+        self.config.set('OBSWebSocket', 'serverenabled', value)
+
+    @property
+    def websocket_port(self):
+        return self.config.get('OBSWebSocket', 'ServerPort')
+
+    @websocket_port.setter
+    def websocket_port(self, value):
+        self.config.set('OBSWebSocket', 'ServerPort', value)
+
+    @property
+    def websocket_password(self):
+        return self.config.get('OBSWebSocket', 'ServerPassword')
+
+    @websocket_password.setter
+    def websocket_password(self, value):
+        self.config.set('OBSWebSocket', 'ServerPassword', value)
+
+    def save(self):
+        with open(self.ini_path, 'w', encoding='utf-8') as f:
+            self.config.write(f)
--- program/recorder.py
+++ program/recorder.py
@@ -2,15 +2,12 @@ import subprocess
 import os 
 import logging
 logging.basicConfig(level=logging.ERROR)
-OBS_DIR_PATH = "C:\\Program Files\\obs-studio\\bin\\64bit\\"
-OBS_EXE_NAME = "obs64.exe"
 
-os.chdir(OBS_DIR_PATH)
-result = subprocess.run(OBS_EXE_NAME , shell=True, capture_output=True)
-
-if result.returncode == 0:
-    print("OBS Opened.")
-else:
-    subprocess.run(["winget", "install", "OBSProject.OBSStudio"], shell=True, capture_output=True)
-    logging.ERROR(f"Failed to open the executable. Return code: {result.returncode}")
-    
+def is_obs_installed(dir, name):
+    os.chdir(dir)
+    result = subprocess.run(name, shell=True, capture_output=True)
+    if result.returncode == 0:
+        print("OBS Opened.")
+    else:
+        subprocess.run(["winget", "install", "OBSProject.OBSStudio"], shell=True, capture_output=True)
+        logging.ERROR(f"Failed to open the executable. Return code: {result.returncode}")
