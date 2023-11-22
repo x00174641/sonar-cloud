d004860966f50723b659631ae6b0335d2f311a22
Jason Fung <x00174641@mytudublin.ie>
2023-11-22 02:49:03 +0000

    Added actions to start recording and stop recording added few more checks

--- program/recorder.py
+++ program/recorder.py
@@ -1,7 +1,18 @@
 import subprocess
 import os
 import logging
+import time
+import getpass
+import obsws_python as obs
+import psutil
+from config import OBSConfig
 logging.basicConfig(level=logging.ERROR)
+username = getpass.getuser()
+OBS_CONFIG_PATH = f"C:\\Users\\{username}\\AppData\\Roaming\\obs-studio"
+OBS_CONFIG_NAME = "global.ini"
+
+def setup():
+    os.makedirs(f"C:\\Users\\{username}\\Videos\\CLIPR_raw_videos")
 
 def is_obs_installed(directory):
     if not os.path.isdir(directory):
@@ -33,3 +44,36 @@ def first_load_obs(directory, name):
                    shell=True,
                    )
     process.terminate()
+
+def ws_connection(): 
+    # OBS Settings
+    host = "localhost"
+    obs_config = OBSConfig(OBS_CONFIG_PATH,OBS_CONFIG_NAME)
+    obs_ws_status = obs_config.websocket_status
+    obs_websocket_port = int(obs_config.websocket_port)
+    obs_websocket_password = obs_config.websocket_password
+    print(type(obs_websocket_password))
+
+    if obs_ws_status == 'false':
+        obs_config.websocket_status = 'true'
+        obs_config.save()
+
+    ws = obs.ReqClient(host=host, port=obs_websocket_port, password=obs_websocket_password, timeout=3)
+    wait_for_obs()
+    if not os.path.exists(f"C:\\Users\\{username}\\Videos\\CLIPR_raw_videos"):
+        setup()
+        ws.set_record_directory(f"C:\\Users\\{username}\\Videos\\CLIPR_raw_videos")
+
+    return ws
+
+def wait_for_obs():
+    start_time = time.time()
+    while True:
+        for process in psutil.process_iter(attrs=['name']):
+            if process.info['name'] == 'obs64.exe' or process.info['name'] == 'obs32.exe':
+                print("OBS is now running.")
+                return
+        if time.time() - start_time >= 60: 
+            print("Timeout reached. OBS is not running.")
+            return
+        time.sleep(0.1)
