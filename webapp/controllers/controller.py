a686a63bc2c4a42470605fc2f09ba22adafb2b38
Jason Fung <x00174641@mytudublin.ie>
2024-01-19 19:13:59 +0000

    Added video display by videoID stored on dynamoDB

--- webapp/controllers/controller.py
+++ webapp/controllers/controller.py
@@ -5,12 +5,17 @@ import hashlib
 import base64
 from botocore.exceptions import ClientError
 from webapp import app
-from flask import render_template, request, flash, redirect, url_for , session
+from flask import render_template, request, flash, redirect, url_for , session, jsonify
 from webapp.models.models import create_user
+from boto3.dynamodb.conditions import Attr
 
 client_id = os.getenv('COGNITO_APP_CLIENT_ID')
 client_secret = os.getenv('COGNITO_APP_CLIENT_SECRET')
 client = boto3.client('cognito-idp', "us-east-1")
+access_key = os.getenv('ACCESS_KEY')
+access_secret = os.getenv('SECRET_KEY')
+dynamodb = boto3.resource('dynamodb',"us-east-1", aws_access_key_id=access_key, aws_secret_access_key=access_secret)
+table = dynamodb.Table('cliprDB')
 
 # Routes 
 @app.route('/')
@@ -70,3 +75,20 @@ def login():
                 flash('Login failed. Please check your username and password.', 'error')
                 print(f"Error: {e}")
     return render_template('login.html')
+
+@app.route('/videos/<videoID>')
+def video(videoID):
+    try:
+        response = table.scan(
+            FilterExpression=Attr('videos').contains("videos/" + videoID)
+        )
+        items = response.get('Items', [])
+        if not items:
+            return render_template('index.html') # replace with 404 page once i build that
+        else:
+            username = items[0].get('username')
+            video_url = f"https://cliprbucket.s3.amazonaws.com/videos/videos/{videoID}"
+        return render_template('videos.html', video_url=video_url, username=username)
+
+    except Exception as e:
+        return str(e), 500
