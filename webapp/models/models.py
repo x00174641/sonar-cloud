1a721e207d0d98ebcdd2bf80d9658089c11443cb
Jason Fung <x00174641@mytudublin.ie>
2024-01-21 16:53:46 +0000

    Added user profiles, added new db field channelName for URI

--- webapp/models/models.py
+++ webapp/models/models.py
@@ -34,7 +34,7 @@ def create_user(username, email, password):
             UserAttributes=user_attributes,
             SecretHash=secret_hash
         )
-        table.put_item(Item={'username': username, 'email': email, 'videos': []})
+        table.put_item(Item={'username': username, 'channelName': "@" + username.lower() ,'email': email, 'videos': []})
 
         return True
     except ClientError as e:
