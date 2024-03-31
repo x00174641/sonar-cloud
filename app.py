9024ae50ae9a27ed4bd0d1670902ceecc9b538b5
Jason Fung <x00174641@mytudublin.ie>
2024-03-31 15:28:10 +0100

    Added new route to flask

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from webapp import app
 from flask_cors import CORS
 CORS(app, resources={r"/login": {"origins": "http://localhost:5173"}})
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
     
