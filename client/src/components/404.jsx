562ff65a99315a694b920c2fc2bd9c40ac9f4dcc
Jason Fung <x00174641@mytudublin.ie>
2024-04-18 01:35:19 +0100

    .

--- client/src/components/404.jsx
+++ client/src/components/404.jsx
@@ -7,7 +7,7 @@ function NotFound() {
 
         <div>
             <Container2>
-            <h1 className="not-found-text mt-20">404</h1>
+            <h1 className="not-found-text mt-20">404 Not</h1>
             <p className='text-xl'>Looks like you're lost.</p>
             <p>The page you are looking for could not be found.</p>
             <a href="/"><Button variant="outline" className="mt-10">RETURN HOME</Button></a>
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -57,7 +57,7 @@ function HomePage() {
                         speed={150}
                         repeat={Infinity}
                     />
-                    <small className='text-muted-foreground text-lg'>Cliprs is a user-friendly tool for recording and trimming content, enabling users to capture and share highlights effortlessly via generated links.</small>
+                    <small className='text-muted-foreground text-lg'>Cliprsfasfasdfas is a user-friendly tool for recording and trimming content, enabling users to capture and share highlights effortlessly via generated links.</small>
                     <br></br>
                     <Button style={{ fontSize: "28px" }} size="xl">Download For Windows &#8201; <FaWindows /></Button>
 
