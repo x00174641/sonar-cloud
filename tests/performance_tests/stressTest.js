9414a74ab2ed35abf12ecbb297ef6dc75c613660
Jason Fung <x00174641@mytudublin.ie>
2024-04-19 17:00:20 +0100

    Added stress testing

--- tests/performance_tests/stressTest.js
+++ tests/performance_tests/stressTest.js
@@ -1,11 +1,12 @@
-// code from https://k6.io/docs/test-types/spike-testing/ changed intervals though.
+// Code found here: https://k6.io/docs/test-types/stress-testing/
 import http from 'k6/http';
 import {sleep} from 'k6';
 
 export const options = {
   stages: [
-    { duration: '1m', target: 2000 },
-    { duration: '30s', target: 0 },
+    { duration: '1m', target: 200 },
+    { duration: '2m', target: 200 }, 
+    { duration: '30s', target: 0 }, 
   ],
 };
 
@@ -13,3 +14,4 @@ export default () => {
   const urlRes = http.get('https://staging.clipr.solutions/');
   sleep(1);
 };
+
