b41e6a2fe622b14bec906cf699744aef483fd67c
Jason Fung <x00174641@mytudublin.ie>
2024-04-20 04:08:44 +0100

    Upgraded software infos and logic

--- program/main.py
+++ program/main.py
@@ -2,22 +2,63 @@ import logging
 import keyboard
 import time
 import getpass
-from recorder import is_obs_installed, open_obs, ws_connection, wait_for_obs, process_latest_video, is_ffmpeg_installed
+from recorder import is_obs_installed, open_obs, ws_connection, process_latest_video
 from request import API_FETCH_USER_SETTINGS
 username = getpass.getuser()
 
+
+def print_colored_text(text):
+    CYAN = '\033[96m'
+    WHITE = '\033[97m'
+    for char in text:
+        if char == 'l':
+            print(CYAN + char, end='')
+        elif char == 'i':
+            print(CYAN + char, end='')
+        elif char == 'p':
+            print(CYAN + char, end='')
+        elif char == 'c':
+            print(CYAN + char, end='')
+        elif char == 'r':
+            print(CYAN + char, end='')
+        elif char == ' ':
+            print(CYAN + char, end='')
+        else:
+            print(WHITE + char, end='')
+
+text = """                                                    
+                    lllllll   iiii                                         
+                    l:::::l  i::::i                                        
+                    l:::::l   iiii                                         
+                    l:::::l                                                
+    cccccccccccccccc l::::l iiiiiiippppp   ppppppppp   rrrrr   rrrrrrrrr   
+  cc:::::::::::::::c l::::l i:::::ip::::ppp:::::::::p  r::::rrr:::::::::r  
+ c:::::::::::::::::c l::::l  i::::ip:::::::::::::::::p r:::::::::::::::::r 
+c:::::::cccccc:::::c l::::l  i::::ipp::::::ppppp::::::prr::::::rrrrr::::::r
+c::::::c     ccccccc l::::l  i::::i p:::::p     p:::::p r:::::r     r:::::r
+c:::::c              l::::l  i::::i p:::::p     p:::::p r:::::r     rrrrrrr
+c:::::c              l::::l  i::::i p:::::p     p:::::p r:::::r            
+c::::::c     ccccccc l::::l  i::::i p:::::p    p::::::p r:::::r            
+c:::::::cccccc:::::cl::::::li::::::ip:::::ppppp:::::::p r:::::r            
+ c:::::::::::::::::cl::::::li::::::ip::::::::::::::::p  r:::::r            
+  cc:::::::::::::::cl::::::li::::::ip::::::::::::::pp   r:::::r            
+    cccccccccccccccclllllllliiiiiiiip::::::pppppppp     rrrrrrr            
+                                    p:::::p                                
+                                    p:::::p                                
+                                   p:::::::p                               
+                                   p:::::::p                               
+                                   p:::::::p                               
+                                   ppppppppp
+"""
+
 def main():
-    
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.ERROR)
         # Paths
         OBS_DIR_PATH = "C:\\Program Files\\obs-studio\\bin\\64bit\\"
-        FFMPEG_DIR_PATH = f"C:\\Users\\{username}\\AppData\\Local\\Microsoft\\WinGet\\Links"
         OBS_EXE_NAME = "obs64.exe"
-
+        print(print_colored_text(text))
         is_obs_installed(OBS_DIR_PATH)
-        is_ffmpeg_installed(FFMPEG_DIR_PATH)
         open_obs(OBS_DIR_PATH, OBS_EXE_NAME)
-        wait_for_obs()
         try:
             ws = ws_connection()
             ws.start_record()
@@ -26,9 +67,10 @@ def main():
             keyboard.wait('ctrl+shift+f2')
             if keyboard.is_pressed("ctrl+shift+f2"):
                 ws.stop_record()
+                exit()
         except KeyboardInterrupt:
             ws.stop_record()
-            pass
+            exit()
             
 if __name__ == "__main__":
     main()
--- program/recorder.py
+++ program/recorder.py
@@ -7,25 +7,24 @@ import obsws_python as obs
 import psutil
 import glob
 import uuid
+import ffmpeg
+from moviepy.editor import VideoFileClip
 from config import OBSConfig
 from request import upload_video_to_s3, API_FETCH_USER_SETTINGS, postPassword
-logging.basicConfig(level=logging.ERROR)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 username = getpass.getuser()
 OBS_CONFIG_PATH = f"C:\\Users\\{username}\\AppData\\Roaming\\obs-studio"
 OBS_CONFIG_NAME = "global.ini"
-
+CYAN = '\033[96m'
+RESET = '\033[0m'
 def setup():
-    os.makedirs(f"C:\\Users\\{username}\\Videos\\CLIPR_raw_videos")
-    os.makedirs(f"C:\\Users\\{username}\\Videos\\CLIPR_clips")
+    os.makedirs(f"C:\\CLIPR_raw_videos")
+    os.makedirs(f"C:\\CLIPR_clips")
 
 def is_obs_installed(directory):
     if not os.path.isdir(directory):
         install_obs()
-
-def is_ffmpeg_installed(directory):
-    ffmpeg_path = os.path.join(directory, 'ffmpeg.exe')
-    if not os.path.isfile(ffmpeg_path):
-        install_ffmpeg()
 
 def install_obs():
     try:
@@ -35,25 +34,12 @@ def install_obs():
             check=True
         )
         if result.returncode == 0:
-            print("OBS Studio installed successfully.")
+            logger.info("OBS Studio installed successfully.")
         else:
-            print("OBS Studio installation failed with return code %s", {result.returncode})
+            logger.error("OBS Studio installation failed with return code %s", {result.returncode})
     except subprocess.CalledProcessError as e:
-        logging.error("An error occurred while trying to install OBS Studio: %s", e)
+        logger.error("An error occurred while trying to install OBS Studio: %s", e)
 
-def install_ffmpeg():
-    try:
-        result = subprocess.run(
-            ["winget", "install", "Gyan.FFmpeg"],
-            shell=True,
-            check=True
-        )
-        if result.returncode == 0:
-            print("FFmpeg installed successfully.")
-        else:
-            print("FFmpeg installation failed with return code %s", {result.returncode})
-    except subprocess.CalledProcessError as e:
-        logging.error("An error occurred while trying to install FFmpeg: %s", e)
 
 def open_obs(directory, name):
     os.chdir(directory)
@@ -79,7 +65,6 @@ def ws_connection():
     obs_ws_status = obs_config.websocket_status
     obs_websocket_port = int(obs_config.websocket_port)
     obs_websocket_password = obs_config.websocket_password
-    print(type(obs_websocket_password))
     if obs_ws_status == 'false':
         obs_config.websocket_status = 'true'
         obs_config.save()
@@ -87,9 +72,9 @@ def ws_connection():
     ws = obs.ReqClient(host=host, port=obs_websocket_port, password=obs_websocket_password, timeout=3)
     postPassword(obs_websocket_password)
     wait_for_obs()
-    if not os.path.exists(f"C:\\Users\\{username}\\Videos\\CLIPR_raw_videos"):
+    if not os.path.exists(f"C:\\CLIPR_raw_videos"):
         setup()
-        ws.set_record_directory(f"C:\\Users\\{username}\\Videos\\CLIPR_raw_videos")
+        ws.set_record_directory(f"C:\\CLIPR_raw_videos")
     return ws
 
 def wait_for_obs():
@@ -97,47 +82,55 @@ def wait_for_obs():
     while True:
         for process in psutil.process_iter(attrs=['name']):
             if process.info['name'] == 'obs64.exe' or process.info['name'] == 'obs32.exe':
-                print("OBS is now running.")
-                return
+                return logger.info("OBS is now running.")
         if time.time() - start_time >= 60: 
-            print("Timeout reached. OBS is not running.")
+            logger.info("Timeout reached. OBS is not running.")
             return
         time.sleep(0.1)
 
 def get_video_duration(filename):
-    """Get the duration of a video in seconds using FFprobe."""
     try:
-        result = subprocess.run(
-            ["ffprobe", "-v", "error", "-show_entries", "format=duration", "-of", "default=noprint_wrappers=1:nokey=1", filename],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT)
-        return float(result.stdout)
+        clip = VideoFileClip(filename)
+        duration = clip.duration
+        clip.close()
+        return duration
     except Exception as e:
-        print(f"Error getting duration: {e}")
+        logger.error(f"Error getting duration: {e}")
         return None
     
 def process_latest_video():
-    print("yes")
-    raw_folder_path = f"C:\\Users\\{username}\\Videos\\CLIPR_raw_videos"
-    clips_folder_path = f"C:\\Users\\{username}\\Videos\\CLIPR_clips"
+    logging.info('Processing video for upload to S3.')
+    raw_folder_path = "C:\\CLIPR_raw_videos"
+    clips_folder_path = "C:\\CLIPR_clips"
 
-    file_type = '\\*.mp4'
-    files = glob.glob(raw_folder_path + file_type)
+    file_type = '*.mp4'
+    files = glob.glob(os.path.join(raw_folder_path, file_type))
     latest_video = max(files, key=os.path.getctime)
 
     try:
         duration = get_video_duration(latest_video)
         settings = API_FETCH_USER_SETTINGS()
-        clip_interval = int(settings.get('clip_interval', 30)) 
+        clip_interval = int(settings.get('clip_interval', 30))
 
         start_time = duration - clip_interval
+
         random_url = uuid.uuid4().hex
         output_filename = os.path.join(clips_folder_path, f"{random_url}.mp4")
-        subprocess.run(["ffmpeg", "-ss", str(start_time), "-i", latest_video, "-t", str(clip_interval), "-c", "copy", output_filename], check=True)
-        print(f"Video trimmed successfully and saved as {output_filename}")
-        upload_video_to_s3(output_filename, f"videos/{random_url}")
-        print(random_url)
-
+        if duration > clip_interval:
+            (
+                ffmpeg
+                .input(latest_video, ss=start_time)
+                .output(output_filename, t=clip_interval, c='copy')
+                .run(overwrite_output=True)
+            )
+            logger.info("Video trimmed successfully and saved as" + CYAN + f"{output_filename}")
+            print(RESET)
+            upload_video_to_s3(output_filename, f"videos/{random_url}")
+        else:
+            upload_video_to_s3(latest_video, f"videos/{random_url}")
+        
+        logger.info("Clip uploaded to Clipr: " + CYAN + f"https://clipr.solutions/clip/{random_url}")
+        print(RESET)
     except Exception as e:
-        print(e)
-        logging.error(f"Error processing video: {e}")
+        
+        logger.error(f"Error processing video: {e}")
